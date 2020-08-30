a167d8d94e392470bf3148b35c7e55a7976dcc3f
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:18:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  i=0
+    while i > num.length
+    i+=1
+      if num[i-1] > num[i]
+        num[i], num[i-1] = num[i-1], array[i]
+      else
+        break
+      end
+      
+    end
+  
