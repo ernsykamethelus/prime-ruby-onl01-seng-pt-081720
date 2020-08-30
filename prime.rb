a9036d1c5a1c2d9bc2f04e34ed14d14659354079
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:32:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,2 @@
-def isPrime(n): 
-      
-    # Corner case 
-    if (n <= 1): 
-        return False
+def isPrime(num)=
   
-    # Check from 2 to n-1 
-    for i in range(2, n): 
-        if (n % i == 0): 
-            return False
-  
-    return True
-  
-# Driver Program  
-if isPrime(11): 
-    print ("true") 
-else: 
-    print ("false") 
