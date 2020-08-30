3c0923f83db9b5f1aebf0934aa0f898c14fd387b
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:29:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,9 @@
-def prime?(num)
-  i=0
-    while i > num
-    i+=1
-      if num[i-1] > num[i]
-        num[i], num[i-1] = num[i-1], array[i]
-      else
-        break
-      end
+def is_prime?(num)
+    flag = true
+    for x in 2..num-1
+        if num % x == 0
+            flag = false
+        end
     end
-  end
-
-      def prime?(num)
- (2..(num - 1)).each do |n|
-  return false if num % n == 0
-  end
-  true
-end
-  
-  if num.abs <2   
-    return false     
-elsif num.abs % counter == 0 && num.abs!=counter  
-    return false
-else 
-    return true 
+    return flag
 end
