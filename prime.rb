84720e0a2309a436d36f2b470018ed55e7ca4727
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:47:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
-def prime?(num)
- (2..(num - 1)).each do |n|
-  return false if num % n == 0
-  end
-  true
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
-
