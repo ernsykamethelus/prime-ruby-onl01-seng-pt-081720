19afebe02eb9bbe01aed8d5e71425abbcbd176c6
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:25:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,9 @@
 def prime?(num)
-  i=0
-    while i > num
-    i+=1
-      if num[i-1] > num[i]
-        num[i], num[i-1] = num[i-1], array[i]
-      else
-        break
-      end
-    end
+ (2..(num - 1)).each do |n|
+  return false if num % n == 0
   end
-
+  true
+end
+  
       
   
