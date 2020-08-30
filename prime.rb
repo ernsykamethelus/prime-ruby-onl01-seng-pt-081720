243bc41f6f51dbbbdbb7a7ae1c6f0cf84bb1ebc8
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 06:30:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 def is_prime?(num)
-    flag = true
-    for x in 2..num-1
-        if num % x == 0
-            flag = false
-        end
-    end
-    return flag
-end
+   for x in 2..num-1
+      print x
+      if num % x == 0
+         return false
+      else
+         return true
+      end
+   end
+end 
